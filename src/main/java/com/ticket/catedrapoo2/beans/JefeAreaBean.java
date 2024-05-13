3118fb204264179e96f651a6c738f6d221ff8ef0
larata365@gmail.com <yAeroo>
2024-05-13 04:33:58 -0600

    Hola soy nuevo Todas las funciones del empleado de área funcional finalizadas jeje owo

---
+++ src/main/java/com/ticket/catedrapoo2/beans/JefeAreaBean.java
@@ -0,0 +1,11 @@
+package com.ticket.catedrapoo2.beans;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class JefeAreaBean {
+
+    // Obteniendo tickets asignados
+}
